0cc67448153908ac9133a05a7e3cc9400058d6ad
Ketan Chaudhari <kriot2016@gmail.com>
2021-12-02 12:43:54 +0530

    C_and_C_Programs

---
+++ Second_Program/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a=20;
+    char b='A';
+    double c=20.33;
+    printf("%d %c %lf\n",20,'Z',20.33);
+    printf("%d,%c,%lf\n",a,b,c);
+    int num1=30,num2=40;
+    printf("%d\n%d",num1,num2);
+    return 0;
+}
