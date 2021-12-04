369333c462867f45bb7533197dd950bc9fa8a359
Ketan Chaudhari <kriot2016@gmail.com>
2021-12-04 09:12:03 +0530

    C_and_C_Programs

--- Second_Program/main.c
+++ Second_Program/main.c
@@ -4,7 +4,7 @@ int main(){
     char b='A';
     double c=20.33;
     printf("%d %c %lf\n",20,'Z',20.33);
-    printf("%d,%c,%lf\n",a,b,c);
+    printf("%d %c %lf\n",a,b,c);
     int num1=30,num2=40;
     printf("%d\n%d",num1,num2);
     return 0;
