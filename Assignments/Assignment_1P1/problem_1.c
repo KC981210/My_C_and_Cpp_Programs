57f5e5d5b986b26e49ea34ae2ca0f26d8d76e556
Ketan Chaudhari <kriot2016@gmail.com>
2021-12-04 09:26:19 +0530

    C_an_C_Programs

--- Assignments/Assignment_1P1/problem_1.c
+++ Assignments/Assignment_1P1/problem_1.c
@@ -8,25 +8,25 @@ int main()
     int diff;
     int prod;
     */
-    /* unsigned int num1;
+    unsigned int num1;
     unsigned int num2;
     unsigned int sum;
     unsigned int diff;
     unsigned int prod;
-    */
-   /* signed int num1;
+    
+   /*signed int num1;
     signed int num2;
     signed int sum;
     signed int diff;
     signed int prod;
     */
    
-    char num1;
+    /*char num1;
     char num2;
     char sum;
     char diff;
     char prod;
-    
+    */
 
     /*long int num1;
     long int num2;
@@ -42,14 +42,19 @@ int main()
     diff=num1-num2;
     prod=num1*num2;
 
-    printf("Sum of two numbers is %d\n",sum);
+    /*printf("Sum of two numbers is %d\n",sum);
     printf("Difference between two numbers is %d\n",diff);
     printf("Product of two numbers is %d\n",prod);
-    
+    */
 
-    /*printf("Sum of two numbers is %u\n",sum);
+    printf("Sum of two numbers is %u\n",sum);
     printf("Difference between two numbers is %u\n",diff);
     printf("Product of two numbers is %u",prod);
+    
+
+    /*printf("Sum of two numbers is %c\n",sum);
+    printf("Difference between two numbers is %c\n",diff);
+    printf("Product of two numbers is %c\n",prod);
     */
 
    /* printf("Sum of two numbers is %ld\n",sum);
