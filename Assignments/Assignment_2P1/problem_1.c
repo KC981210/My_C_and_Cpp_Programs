fb7da49770b10f9ae80afa66cd036aabed5fc116
Ketan Chaudhari <kriot2016@gmail.com>
2021-12-05 21:38:26 +0530

    C_C_Programs

---
+++ Assignments/Assignment_2P1/problem_1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("%d\n",'\r');
+    printf("%d\n",'\v');
+    printf("%d\n",'\n');
+    printf("%d\n",'\t');
+    printf("%d\n",'\a');
+    printf("%d\n",'\b');
+    return 0;    
+}
