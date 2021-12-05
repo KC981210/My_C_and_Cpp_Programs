b18af241e6531b8eb3796669059a0f8d359f7bc9
Ketan Chaudhari <kriot2016@gmail.com>
2021-12-05 22:47:41 +0530

    C_C_Programs

---
+++ Assignments/Assignment_3P1/problem_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int num,a,b,c,sum;
+    printf("Enter three digit number : ");
+    scanf("%d",&num);
+    a=num/100;
+    b=(num-a*100)/10;
+    c=(num-a*100-b*10);
+    sum=a+b+c;
+    printf("Sum of all digits is : %d",sum);
+    return 0;
+}
