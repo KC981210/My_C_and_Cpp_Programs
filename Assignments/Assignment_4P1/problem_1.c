0f5c281f0ee200f4f0045096d7928ca98081b201
Ketan Chaudhari <kriot2016@gmail.com>
2021-12-06 23:22:55 +0530

    C_C_Program

--- Assignments/Assignment_4P1/problem_1.c
+++ Assignments/Assignment_4P1/problem_1.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-
+/*
+Write a program to accept a 5 digit number and check whether it is
+a numeric palindrome. (If reversed number is same as entered
+number it is called palindrome).
+*/
 int main()
 {
     int num,a,b,c,d,e,palidrome;
